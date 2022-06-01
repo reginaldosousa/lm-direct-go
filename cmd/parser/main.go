ba0a502c2b8b90bf84d60436ba8f0c94e60929af
Reginaldo Sousa <reginaldo.rms@gmail.com>
2022-06-01 15:47:00 -0300

    add parser cli

---
+++ cmd/parser/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"lm-direct-go/messages"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: lm_parser MESSAGE")
+		fmt.Println("Ex: lm_parser 83050102030405010102010001000000363067")
+		os.Exit(1)
+	}
+	h := os.Args[1]
+	decodedBytes, err := hex.DecodeString(h)
+	if err != nil {
+		log.Fatal(err)
+	}
+	buf := bytes.NewBuffer(decodedBytes)
+
+	message, err := messages.Parse(buf)
+	if err != nil {
+		log.Fatalf("fail to parse message: %s", err)
+	}
+	formatted, err := json.MarshalIndent(&message, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", formatted)
+}
